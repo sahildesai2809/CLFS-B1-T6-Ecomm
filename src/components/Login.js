629b746cb3280d5cda828857472bf20ce7467ec5
sruthila <sruthila.vinnu@gmail.com>
2021-10-10 18:47:52 +0530

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,41 @@
-import React, { useRef } from "react";
-import { Form, Button, Card } from "react-bootstrap";
-
-import { Link } from "react-router-dom";
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
 
 export default function Login() {
   const emailRef = useRef();
   const passwordRef = useRef();
+  const { login } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { history } = useHistory();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (emailRef.current.value) {
+      return setError("Email not exist.");
+    }
+    try {
+      setError("");
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      history.push("/");
+    } catch {
+      setError("Failed to login.");
+    }
+
+    setLoading(false);
+  }
 
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Log In</h2>
-
-          <Form>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
@@ -22,7 +44,7 @@ export default function Login() {
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
-            <Button className="w-100" type="submit">
+            <Button disabled={loading} className="w-100" type="submit">
               Log In
             </Button>
           </Form>
